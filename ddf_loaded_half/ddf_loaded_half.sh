07b28690657f5bcb0f59c541941c38e1c821dfbd
Peter Yoachim <yoachim@uw.edu>
2023-09-14 15:14:10 -0700

    ddf half

---
+++ ddf_loaded_half/ddf_loaded_half.sh
@@ -0,0 +1,6 @@
+python ddf_loaded_half.py --ddf_flush_length 1
+python ddf_loaded_half.py --ddf_flush_length 2
+python ddf_loaded_half.py --ddf_flush_length 4
+python ddf_loaded_half.py --ddf_flush_length 8
+python ddf_loaded_half.py --ddf_flush_length 15
+python ddf_half.py
