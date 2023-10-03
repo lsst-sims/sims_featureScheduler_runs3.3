399b28ecd4c4046fb7e2c7013646b59b616aa81c
Peter Yoachim <yoachim@uw.edu>
2023-10-03 11:02:10 -0700

    u exposure time

---
+++ u_exp/README.md
@@ -0,0 +1,2 @@
+Vary the u-band exposure time. Could be an easy way to compensate for the loss in throughput from going to tripple silver.
+
---
+++ u_exp/u_exp.sh
@@ -0,0 +1,5 @@
+python u_exp.py --u_exp 25
+python u_exp.py --u_exp 30
+python u_exp.py --u_exp 40
+python u_exp.py --u_exp 50
+python u_exp.py --u_exp 60
