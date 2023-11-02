bb5307dc481c3ec1ffedbbaaa5b6c4315af2f7b4
Matthew R. Becker <beckermr@users.noreply.github.com>
2023-11-02 15:22:01 -0500

    Update rolling_phase.sh

--- rolling_phase/rolling_phase.sh
+++ rolling_phase/rolling_phase.sh
@@ -1,4 +1,4 @@
 python rolling_phased.py --phase_shift 0
 python rolling_phased.py --phase_shift 182.625
 python rolling_phased.py --phase_shift -182.625
-python rolling_phased.py --phase_shift 182.625 --phase_shift_first 91.3125
+python rolling_phased.py --phase_shift 182.625 --phase_shift_first -91.3125
