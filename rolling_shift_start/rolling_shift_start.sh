ad7f7721b4ea0ee9700b25f2bd2d5ac21927c4b4
beckermr <becker.mr@gmail.com>
2023-11-06 17:45:38 -0600

    ENH try shifting the start of the survey

---
+++ rolling_shift_start/rolling_shift_start.sh
@@ -0,0 +1,4 @@
+python rolling_shift_start.py --scenario "sh1_0_sh2_0_sh3_0"
+python rolling_shift_start.py --scenario "sh1_0_sh2_0.5_sh3_1"
+python rolling_shift_start.py --scenario "sh1_-0.25_sh2_0.5_sh3_1"
+python rolling_shift_start.py --scenario "sh1_-0.25_sh2_0.25_sh3_0.75"
